1df09a8464eda52e0b5c467efc958fbfa36f540f
Chris Narrikkattu <cnarrikk@users.noreply.github.com>
2015-04-17 23:07:22 -0400

    add basic tests, fix singleton_search, add checker

--- sudokusolver.cu
+++ sudokusolver.cu
@@ -21,6 +21,10 @@ enum {
     STAT_NOTOK = 2
 };
 
+enum {
+    SUFLG_PROPAGATE_SINGLE = (1<<0), //propagated the fact that I solved this cell already
+};
+
 template<int SIZE>
 struct SudokuProblem {
     uint32_t givens[SIZE][SIZE]; //0 if unknown, digit otherwise
@@ -30,6 +34,7 @@ struct SudokuProblem {
 template<int SIZE>
 struct SudokuState {
     uint32_t bitstate[SIZE][SIZE];
+    uint8_t  work_flag[SIZE][SIZE]; //flags to cache work done
 };
 
 template<int SIZE>
@@ -57,13 +62,16 @@ __global__ void simple_cand_elim(SudokuState<RSIZE*RSIZE> *p, int *rc) {
     //copy current values
     const uint32_t myval = p->bitstate[r][c];
     s.bitstate[r][c] = myval; 
+    s.work_flag[r][c] = p->work_flag[r][c];
     __syncthreads();
     if(myval == 0) {
+        block_ok = 0;
         goto ending;
     }
     if(myval & (myval-1)) {
         goto ending;
     }
+    if(s.work_flag[r][c] & SUFLG_PROPAGATE_SINGLE) {goto ending;}
     //here we have a singleton!!, atomically update neighbors
     //row
     for(int oc=0;oc<RSIZE*RSIZE;++oc) {
@@ -99,6 +107,7 @@ __global__ void simple_cand_elim(SudokuState<RSIZE*RSIZE> *p, int *rc) {
             }
         }
     }
+    p->work_flag[r][c] |= SUFLG_PROPAGATE_SINGLE;
 ending:;
     __syncthreads();
     p->bitstate[r][c] = s.bitstate[r][c];
@@ -194,16 +203,17 @@ __global__ void singleton_search(SudokuState<RSIZE*RSIZE> *p, int *rc) {
         }
     }
 ending:
-    if(ok && finalval != 0) {
+    //either we broke something, or we concluded that two values need to fit?
+    if(!ok || (finalval != 0 && (finalval & (finalval-1)) != 0)) {
+        //we can do this since the change is in one direction,
+        //and we sync before reading it
+        block_ok = 0;
+    }
+    else if(finalval != 0) { //implies ok && finalval has 1 bit set
         p->bitstate[r][c] = finalval;
         //we can do this since the change is in one direction,
         //and we sync before reading it
         block_status = STAT_UPDATED;
-    }
-    else {
-        //we can do this since the change is in one direction,
-        //and we sync before reading it
-        block_ok = 0;
     }
     __syncthreads();
     if(r == 0 && c == 0) {
@@ -216,6 +226,7 @@ ending:
 
 template<int SIZE>
 void fill_state_from_problem(SudokuState<SIZE> *state, const SudokuProblem<SIZE> &problem) {
+    memset(state, 0, sizeof(SudokuState<SIZE>));
     for(int r=0;r<SIZE;++r) {
         for(int c=0;c<SIZE;++c) {
             if(problem.givens[r][c] != 0) {
@@ -228,6 +239,68 @@ void fill_state_from_problem(SudokuState<SIZE> *state, const SudokuProblem<SIZE>
             }
         }
     }
+}
+
+template<int RSIZE>
+void check_state(const SudokuState<RSIZE*RSIZE> &s) {
+    bool ok = true;
+    for(int r=0;r<RSIZE*RSIZE;++r) {
+        for(int c=0;c<RSIZE*RSIZE;++c) {
+            const uint32_t bs = s.bitstate[r][c];
+            if(bs == 0 || (bs & (bs-1)) != 0) {
+                std::cerr << "Row " << r << " " << "col " << c << " not singleton!" << std::endl;
+                ok = false;
+            }
+        }
+    }
+    if(!ok){return;}
+    //row check
+    const uint32_t GOAL = (1u << (RSIZE*RSIZE)) - 1;
+    for(int r=0;r<RSIZE*RSIZE;++r) {
+        uint32_t xx = 0;
+        for(int i=0;i<9;++i) {
+            const int nr = r;
+            const int nc = i;
+            xx |= s.bitstate[nr][nc];
+        }
+        if(xx != GOAL) {
+            std::cerr << "Row " << r << " is no good" << std::endl;
+            ok = false;
+            break;
+        }
+    }
+    if(!ok){return;}
+    for(int c=0;c<RSIZE*RSIZE;++c) {
+        uint32_t xx = 0;
+        for(int i=0;i<9;++i) {
+            const int nr = i;
+            const int nc = c;
+            xx |= s.bitstate[nr][nc];
+        }
+        if(xx != GOAL) {
+            std::cerr << "Col " << c << " is no good" << std::endl;
+            ok = false;
+            break;
+        }
+    }
+    if(!ok){return;}
+    for(int br=0;br<RSIZE && ok;++br) {
+        for(int bc=0;bc<RSIZE;++bc) {
+            uint32_t xx = 0;
+            for(int i=0;i<9;++i) {
+                const int nr = RSIZE*br + (i/3);
+                const int nc = RSIZE*bc + (i%3);
+                xx |= s.bitstate[nr][nc];
+            }
+            if(xx != GOAL) {
+                std::cerr << "Region " << br << "," << bc << " is no good" << std::endl;
+                ok = false;
+                break;
+            }
+        }
+    }
+    if(!ok){return;}
+    std::cerr << "ALL GOOD!" << std::endl;
 }
 
 void test_basics(SudokuState<9> &state) {
@@ -245,21 +318,33 @@ void test_basics(SudokuState<9> &state) {
     dim3 threads_per_block(9,9,1);
     for(h_rc = STAT_UPDATED;h_rc == STAT_UPDATED;)
     {
+        h_rc = STAT_NOCHG;
+        GPU_CHECKERROR(cudaMemset(d_rc, 0, sizeof(int)));
         //singleton_search<3><<<num_block, threads_per_block>>>(d_state, d_rc);
         simple_cand_elim<3><<<num_block, threads_per_block>>>(d_state, d_rc);
         GPU_CHECKERROR(cudaGetLastError());
         GPU_CHECKERROR(cudaMemcpy(&h_rc, d_rc, sizeof(int), cudaMemcpyDeviceToHost));
         cudaDeviceSynchronize();
-        fprintf(stderr, "GOT RC %d\n", h_rc);
+        fprintf(stderr, "SIMPLE - GOT RC %d\n", h_rc);
+        if(h_rc != STAT_NOCHG){continue;}
+
+        singleton_search<3><<<num_block, threads_per_block>>>(d_state, d_rc);
+        GPU_CHECKERROR(cudaGetLastError());
+        GPU_CHECKERROR(cudaMemcpy(&h_rc, d_rc, sizeof(int), cudaMemcpyDeviceToHost));
+        cudaDeviceSynchronize();
+        fprintf(stderr, "SINGLETON - GOT RC %d\n", h_rc);
+        if(h_rc != STAT_NOCHG){continue;}
+
     }
     GPU_CHECKERROR(cudaMemcpy(&state, d_state, sizeof(SudokuState<9>), cudaMemcpyDeviceToHost));
     cudaDeviceSynchronize();
-    print_state(state);
+    cudaFree(d_state);
+    cudaFree(d_rc);
     
 
     
-    cudaFree(d_state);
-    cudaFree(d_rc);
+    //print_state(state);
+    check_state<3>(state);
 }
 
 
